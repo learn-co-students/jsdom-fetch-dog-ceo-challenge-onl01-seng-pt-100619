c9edbd542a0e88d33313531960f18c2dc2dbcc14
allisoncortez <allizetorc@gmail.com>
2020-05-05 13:29:53 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log('%c HI', 'color: firebrick')
+
+/*document.addEventListener('DomContentLoaded', function() {
+
+    loadImages();
+    loadBreeds();
+});
+*/
+
+function loadImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+
+    .then(resp => resp.json())
+    .then(results => {
+        results.message.forEach(image => addImage(image))
+    });
+  }
+
+  function addImage(dogPic) {
+      let container = document.querySelector('#dog-image-container');
+      let NewImage = document.createElement('img');
+      NewImage.src = dogPic;
+      container.appendChild(newImage);
+  }
+
+  function loadBreeds() {
+      const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+      fetch(breedUrl)
+      .then(resp => resp.json())
+      .then(results => {
+          results.message.forEach(breed => addBreed(breed))
+    });
+
+  }
+
+  function addBreed(breed) {
+
+  }
+
