4ccf219a92f5eb3ca44d05246cebefc1689649bb
allisoncortez <allizetorc@gmail.com>
2020-05-05 14:29:08 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,39 +1,46 @@
 
-/*document.addEventListener('DomContentLoaded', function() {
-
+document.addEventListener('DomContentLoaded', function() {
+    // console.log('hi')
+    
     loadImages();
-    loadBreeds();
+    fetchBreeds();
 });
-*/
+
+
 
 function loadImages() {
     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
     fetch(imgUrl)
-
     .then(resp => resp.json())
+
+    /*
+    .then(data => {
+        console.log(data)
+    });
+    */
+
     .then(results => {
         results.message.forEach(image => addImage(image))
     });
-  }
+}
+
 
   function addImage(dogPic) {
       let container = document.querySelector('#dog-image-container');
-      let NewImage = document.createElement('img');
-      NewImage.src = dogPic;
+      let newImage = document.createElement('img');
+      newImage.src = dogPic;
       container.appendChild(newImage);
   }
+  
 
-  function loadBreeds() {
+  function fetchBreeds() {
       const breedUrl = 'https://dog.ceo/api/breeds/list/all'
       fetch(breedUrl)
       .then(resp => resp.json())
-      .then(results => {
-          results.message.forEach(breed => addBreed(breed))
-    });
+      .then(function(dogBreeds){renderBreeds(dogBreeds)});
 
   }
 
-  function addBreed(breed) {
-
+  function renderBreeds(dogBreeds){
+      
   }
-
