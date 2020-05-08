50a20efcf5c5cb2ff07f24f05772f49857c930ae
jayson_kesterson <your_email@example.com>
2020-05-08 14:08:40 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
 console.log('%c HI', 'color: firebrick')
+
+function getDogImage() {
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+return fetch(imgUrl)
+  .then(resp => resp.json())
+  .then(function(json) {renderDogImages(json)});
+}
+
+function getDogBreeds() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    return fetch(breedUrl)
+  .then(resp => resp.json())
+  .then(function(json) {renderDogBreeds(json)});
+}
+
+function renderDogImages(json){
+    const imgContainer = document.getElementById('dog-image-container');
+    
+    json["message"].forEach(dogUrl => {
+        const dogImg = document.createElement("img");
+        dogImg.src = dogUrl;
+        imgContainer.appendChild(dogImg);
+        
+    });
+} 
+
+function renderDogBreeds(json){
+    const breedList = document.getElementById('dog-breeds');
+    Object.keys(json["message"]).forEach(breed => {
+        const breedListItem = document.createElement("li");
+        breedListItem.innerText = breed;
+        breedList.appendChild(breedListItem);
+    });
+
+    breedList.childNodes.forEach(listItem => {listItem.addEventListener("click", () =>{
+        listItem.style.color = "#0000FF";
+    })});
+
+};
+
+function filterBreeds() {
+    const dropdown = document.querySelector("select#breed-dropdown")
+  
+    dropdown.addEventListener("change", (e) => {
+      const firstLetter = e.target.value
+      const allBreeds = document.querySelectorAll("li")
+      allBreeds.forEach(breed => {
+        if(breed.innerText.startsWith(firstLetter)) {
+          breed.hidden = false
+        }
+        else {
+          breed.hidden = true
+        }
+      })
+    })
+  }
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    getDogImage();
+    getDogBreeds();
+    filterBreeds();
+  });
